20103286dfb6c9d78ffb56d89b61fdf9218b5cf4
Weston Catron <westonsoccer2000@gmail.com>
2017-02-28 10:59:43 -0500

    Add sorting to tests table.

--- src/components/tests/TestsTable.tsx
+++ src/components/tests/TestsTable.tsx
@@ -11,7 +11,8 @@ export default class TestsTable extends React.Component<{
             description:string,
             channels:Array<String>,
             organizationName: string,
-            tags: any
+            tags: any,
+            range: Array<number>
         }>
         loading: boolean
         loaded: boolean
@@ -26,12 +27,16 @@ export default class TestsTable extends React.Component<{
     onClick?: (test: any) => any,
     prefix: string
 },{
-    currentPrefix: string
+    currentPrefix?: string,
+    sortTag?:string,
+    direction?: number
 }> {
     constructor(props) {
         super(props)
         this.state = {
-            currentPrefix: this.props.prefix
+            currentPrefix: this.props.prefix,
+            sortTag: "CREATED_DATE",
+            direction: 1
         }
         this.props.actions.load(this.state.currentPrefix, this.props.accessToken)
     }
@@ -42,18 +47,43 @@ export default class TestsTable extends React.Component<{
         }
     }
     render() {
+        const direction = (this.state.direction == 1 ? "ascending" : "descending")
         return (
-        <Table selectable>
+        <Table selectable fixed sortable>
             <Table.Header>
                 <Table.Row disabled={this.props.tests.loading}>
-                <Table.HeaderCell sorted="ascending">Name</Table.HeaderCell>
+                <Table.HeaderCell sorted={(this.state.sortTag == "name") ? direction : null } onClick={() => {
+                    this.setState({
+                        sortTag: 'name',
+                        direction: (this.state.sortTag == "name" ? (this.state.direction*-1) : this.state.direction)
+                    })
+                    }}>Name</Table.HeaderCell>
                 <Table.HeaderCell>Description</Table.HeaderCell>
                 <Table.HeaderCell>Channels</Table.HeaderCell>
+                <Table.HeaderCell sorted={(this.state.sortTag == "CREATED_DATE") ? direction : null } onClick={() => {
+                    this.setState({
+                        sortTag: 'CREATED_DATE',
+                        direction: (this.state.sortTag == "CREATED_DATE" ? (this.state.direction*-1) : this.state.direction)
+                    })
+                    }}>Created Date</Table.HeaderCell>
+                <Table.HeaderCell sorted={(this.state.sortTag == "status") ? direction : null } onClick={() => {
+                    this.setState({
+                        sortTag: 'status',
+                        direction: (this.state.sortTag == "status" ? (this.state.direction*-1) : this.state.direction)
+                    })
+                    }}>Status</Table.HeaderCell>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
             {
-                this.props.tests.data.map((row, index) => {
+                this.props.tests.data.sort((a, b) => {
+                    if (this.state.sortTag == "name") {
+                        return a.name > b.name ? -this.state.direction : this.state.direction
+                    }
+                    var tagSort = (a.tags[this.state.sortTag] > b.tags[this.state.sortTag]) ? -this.state.direction : this.state.direction
+                    return tagSort
+                    //return (a.range.length == b.range.length) ? dateSort : ((a.range.length > b.range.length) ? 1 : 0)
+                }).map((row, index) => {
                     return (<Table.Row key={index} disabled={this.props.tests.loading} {...{onClick:() => {
                         if (this.props.onClick) {
                             this.props.onClick(row);
@@ -64,6 +94,8 @@ export default class TestsTable extends React.Component<{
                         <Table.Cell singleLine={true} width={4}>{row.name.substr(`/organizations/${this.props.params.organizationName}/tests/`.length)} {iconForStatus(row.tags.status)}</Table.Cell>
                         <Table.Cell singleLine={false} width={10}>{row.description}</Table.Cell>
                         <Table.Cell width={2}>{row.channels.length}</Table.Cell>
+                        <Table.Cell>{row.tags['CREATED_DATE']}</Table.Cell>
+                        <Table.Cell>{row.tags['status']}</Table.Cell>
                     </Table.Row>)
                 })
             }
