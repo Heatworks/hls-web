70c7b0afce11437dd1392a5d0092dcad5f7838e1
Weston Catron <westonsoccer2000@gmail.com>
2017-04-12 13:46:19 -0400

    Splice log values before sorting.

--- src/components/views/View.tsx
+++ src/components/views/View.tsx
@@ -887,7 +887,7 @@ class LogTable extends React.Component<{
         return (
             <Segment vertical>
                 <Table size="small" compact>
-                    {this.state.values.reverse().map((row, index) => {
+                    {this.state.values.splice(0).reverse().map((row, index) => {
                         return (<Table.Row key={index} active={index == 0} error={row.value.indexOf("[error]") == -1} warning={row.value.indexOf("[warning]") == -1}>
                             <Table.Cell>{row.timestamp}</Table.Cell>
                             <Table.Cell>{row.value}</Table.Cell>
