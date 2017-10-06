9d9ca83ea3bcaafc4904fbbe86c724b0e27d617d
Weston Catron <weston@heatworks.tech>
2017-10-06 14:16:54 -0400

    Setup clearer colors and visuals for channels.

--- src/components/views/View.tsx
+++ src/components/views/View.tsx
@@ -9,6 +9,7 @@ import { valueWithUnit, getUnitForTopic, getTemperatureUnit, UnitLabels } from '
 import { View as ViewModel, ViewRow } from '../../apis/hls_views'
 import { normalizeName } from '../../utils/normalizeNames'
 var moment = require('moment')
+import Color from 'color'
 
 import * as DAC from '../../apis/hls_dac';
 var api_dac = new DAC.DefaultApi()
@@ -1721,91 +1722,98 @@ class M3TriacTempsInSeries extends React.Component<{
         q29: any
     },
     redZone: number,
-    orangeZone: number
+    orangeZone: number,
+    greenZone: number
 },{}> {
     constructor(props) {
         super(props)
     }
     render() {
         const switchHeight = 9.985827;
+        const channelColor = 'white';//"#a5a5a5";
         return (
             <Segment vertical>
                 <svg viewBox="0 0 640 654" width="100%" preserveAspectRatio="true">
                     {/* Outer circle. */}
                     <path d="M 494.4909 158.49932 C 591.35364 255.36157 591.35364 412.40618 494.4909 509.26843 C 397.62864 606.13118 240.58402 606.13118 143.72177 509.26843 C 46.859025 412.40618 46.859025 255.36157 143.72177 158.49932 C 240.58402 61.636568 397.62864 61.636568 494.4909 158.49932" fill="#ced5ff" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
                     {/* Channels. */}
-                    {this.renderChannel("0", 235.25727, 514.38908, 85.03937, 29.19685, "#a5a5a5")}
-                    {this.renderChannel("1", 234.77424, 466.83882, 85.03937, 37.417322, "#a5a5a5")}
-                    {this.renderChannel("2", 234.77424, 442.71742, 85.03937, 13.701459, "#a5a5a5")}
-                    {this.renderChannel("3", 98.711244, 427.15286, 221.10236, 5.9527556, "#a5a5a5")}
-                    {this.renderChannel("4", 98.711244, 408.39334, 221.10236, 8.638621, "#a5a5a5")}
-                    {this.renderChannel("5", 98.711244, 359.03156, 221.10236, 38.131234, "#a5a5a5")}
-                    {this.renderChannel("6", 98.711244, 342.86518, 221.10236, 5.9527556, "#a5a5a5")}
-                    {this.renderChannel("7", 98.711244, 320.90546, 221.10236, 12.36977, "#a5a5a5")}
-                    {this.renderChannel("8", 98.711244, 299.25948, 221.10236, 11.6818995, "#a5a5a5")}
-                    {this.renderChannel("9", 98.711244, 284.2858, 221.10236, 5.9527556, "#a5a5a5")}
-                    {this.renderChannel("10", 98.711244, 266.66105, 221.10236, 8.207862, "#a5a5a5")}
-                    {this.renderChannel("11", 235.57114, 243.55008, 85.03937, 13.589668, "#a5a5a5")}
-                    {this.renderChannel("12", 235.57114, 227.83372, 85.03937, 31.74803, "#a5a5a5")}
-                    {this.renderChannel("13", 235.57114, 186.25874, 85.03937, 31.74803, "#a5a5a5")}
-                    {this.renderChannel("14", 235.57114, 113.18573, 85.03937, 63.212596, "#a5a5a5")}
-                    {this.renderChannel("15", 329.03346, 125.7678, 85.03937, 72.0, "#a5a5a5")}
-                    {this.renderChannel("16", 329.03346, 207.72465, 85.03937, 27.496062, "#a5a5a5")}
-                    {this.renderChannel("17", 330.4338, 246.20625, 85.03937, 5.9527556, "#a5a5a5")}
-                    {this.renderChannel("18", 330.70299, 261.83226, 85.03937, 46.54515, "#a5a5a5")}
-                    {this.renderChannel("19", 329.82711, 317.85588, 85.03937, 6.519685, "#a5a5a5")}
-                    {this.renderChannel("20", 330.70299, 334.80312, 221.10236, 5.9527556, "#a5a5a5")}
-                    {this.renderChannel("21", 330.70299, 350.0207, 221.10236, 5.9527556, "#a5a5a5")}
-                    {this.renderChannel("22", 330.70299, 366.04486, 221.10236, 6.2362206, "#a5a5a5")}
-                    {this.renderChannel("23", 329.82711, 382.5134, 221.10236, 6.519685, "#a5a5a5")}
-                    {this.renderChannel("24", 329.82711, 400.28936, 221.10236, 7.086614, "#a5a5a5")}
-                    {this.renderChannel("25", 329.82711, 418.71455, 85.03937, 45.498196, "#a5a5a5")}
-                    {this.renderChannel("26", 329.82711, 473.00943, 85.03937, 8.503937, "#a5a5a5")}
-                    {this.renderChannel("27", 329.82711, 492.41534, 85.03937, 30.200384, "#a5a5a5")}
+                    {this.renderChannel("0", 235, 514.38908, 84, 29.19685, channelColor)}
+                    {this.renderChannel("1", 235, 466.83882, 84, 37.417322, channelColor)}
+                    {this.renderChannel("2", 235, 442.71742, 84, 13.701459, channelColor)}
+                    {this.renderChannel("3", 100, 427.15286, 219, 5.9527556,channelColor)}
+                    {this.renderChannel("4", 100, 408.39334, 219, 8.638621, channelColor)}
+                    {this.renderChannel("5", 100, 359.03156, 219, 38.131234,channelColor)}
+                    {this.renderChannel("6", 100, 342.86518, 219, 5.9527556,channelColor)}
+                    {this.renderChannel("7", 100, 320.90546, 219, 12.36977, channelColor)}
+                    {this.renderChannel("8", 100, 299.25948, 219, 11.6818995, channelColor)}
+                    {this.renderChannel("9", 100, 284.2858, 219, 5.9527556, channelColor)}
+                    {this.renderChannel("10", 100, 266.66105, 219, 8.207862,channelColor)}
+                    {this.renderChannel("11", 235, 243.55008, 84, 13.589668,channelColor)}
+                    {this.renderChannel("12", 235, 227.83372, 84, 6.0803, channelColor)}
+                    {this.renderChannel("13", 235, 186.25874, 84, 31.74803, channelColor)}
+                    {this.renderChannel("14", 235, 113.18573, 84, 63.212596, channelColor)}
+                    {this.renderChannel("15", 330, 125.7678, 84, 72.0, channelColor)}
+                    {this.renderChannel("16", 330, 207.72465, 84, 27.496062, channelColor)}
+                    {this.renderChannel("17", 330, 246.20625, 84, 5.9527556, channelColor)}
+                    {this.renderChannel("18", 330, 261.83226, 84, 46.54515, channelColor)}
+                    {this.renderChannel("19", 330, 317.85588, 84, 6.519685, channelColor)}
+                    {this.renderChannel("20", 330, 334.80312, 219, 5.9527556, channelColor)}
+                    {this.renderChannel("21", 330, 350.0207, 219, 5.9527556, channelColor)}
+                    {this.renderChannel("22", 330, 366.04486, 219, 6.2362206, channelColor)}
+                    {this.renderChannel("23", 330, 382.5134, 219, 6.519685, channelColor)}
+                    {this.renderChannel("24", 330, 400.28936, 219, 7.086614, channelColor)}
+                    {this.renderChannel("25", 330, 418.71455, 84, 45.498196, channelColor)}
+                    {this.renderChannel("26", 330, 473.00943, 84, 8.503937, channelColor)}
+                    {this.renderChannel("27", 330, 492.41534, 84, 30.200384, channelColor)}
                     {/* Switches. */}
-                    {this.renderSwitch(1, 234.77424, 543.58835, 85.03937, switchHeight, false,  true)}
-                    {this.renderSwitch(2, 234.77424, 504.56992, 85.03937, switchHeight, false, false)}
-                    {this.renderSwitch(3, 234.77424, 457.12076, 85.03937, switchHeight, false, true)}
-                    {this.renderSwitch(4, 98.711244, 433.53212, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(5, 98.711244, 417.27974, 221.10236, switchHeight, false, true)}
-                    {this.renderSwitch(6, 98.711244, 398.32006, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(7, 98.711244, 349.17406, 221.10236, switchHeight, false, true)}
-                    {this.renderSwitch(8, 98.711244, 333.52322, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(9, 98.711244, 311.50835, 221.10236, switchHeight, false, true)}
-                    {this.renderSwitch(10, 98.711244, 290.24926, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(11, 98.711244, 274.86787, 221.10236, switchHeight, false, true)}
-                    {this.renderSwitch(12, 98.711244, 257.24404, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(13, 236.44936, 233.64696, 85.03937, switchHeight, false, true)}
-                    {this.renderSwitch(14, 236.44936, 218.49986, 85.03937, switchHeight, false, false)}
-                    {this.renderSwitch(15, 236.44936, 176.61637, 85.03937, switchHeight, false, true)}
-                    {this.renderSwitch(16, 236.44936, 104.25976, 85.03937, switchHeight, false, false)}
+                    {this.renderSwitch(1, 234, 543.58835, 86, switchHeight, false,  true)}
+                    {this.renderSwitch(2, 234, 504.56992, 86, switchHeight, false, false)}
+                    {this.renderSwitch(3, 234, 457.12076, 86, switchHeight, false, true)}
+                    {this.renderSwitch(4, 99, 433.53212, 221, switchHeight, false, false)}
+                    {this.renderSwitch(5, 99, 417.27974, 221, switchHeight, false, true)}
+                    {this.renderSwitch(6, 99, 398.32006, 221, switchHeight, false, false)}
+                    {this.renderSwitch(7, 99, 349.17406, 221, switchHeight, false, true)}
+                    {this.renderSwitch(8, 99, 333.52322, 221, switchHeight, false, false)}
+                    {this.renderSwitch(9, 99, 311.50835, 221, switchHeight, false, true)}
+                    {this.renderSwitch(10, 99, 290.24926, 221, switchHeight, false, false)}
+                    {this.renderSwitch(11, 99, 274.86787, 221, switchHeight, false, true)}
+                    {this.renderSwitch(12, 99, 257.24404, 221, switchHeight, false, false)}
+                    {this.renderSwitch(13, 234, 233.64696, 86, switchHeight, false, true)}
+                    {this.renderSwitch(14, 234, 218.49986, 86, switchHeight, false, false)}
+                    {this.renderSwitch(15, 234, 176.61637, 86, switchHeight, false, true)}
+                    {this.renderSwitch(16, 234, 104.25976, 86, switchHeight, false, false)}
 
-                    {this.renderSwitch(16, 329.38788, 116.75976, 85.03937, switchHeight, false, false)}
-                    {this.renderSwitch(17, 329.38788, 198.38484, 85.03937, switchHeight, false, true)}
-                    {this.renderSwitch(18, 330.70299, 235.89022, 85.03937, switchHeight, false, false)}
-                    {this.renderSwitch(19, 329.55212, 251.9803, 85.03937, switchHeight, false, true)}
-                    {this.renderSwitch(20, 330.70299, 308.37741, 85.03937, switchHeight, false, false)}
-                    {this.renderSwitch(21, 330.70299, 325.18706, 221.10236, switchHeight, false, true)}
-                    {this.renderSwitch(22, 330.70299, 341.07932, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(23, 329.80714, 356.28923, 221.10236, switchHeight, false, true)}
-                    {this.renderSwitch(24, 329.80714, 372.50665, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(25, 329.80714, 389.77177, 221.10236, switchHeight, false, true)}
-                    {this.renderSwitch(26, 329.80714, 408.12294, 221.10236, switchHeight, false, false)}
-                    {this.renderSwitch(27, 329.38788, 464.4019, 85.03937, switchHeight, false, true)}
-                    {this.renderSwitch(28, 329.38788, 482.35716, 85.03937, switchHeight, false, false)}
-                    {this.renderSwitch(29, 329.38788, 523.64638, 85.03937, switchHeight, false, true)}
+                    {this.renderSwitch(16, 329, 116.75976, 86, switchHeight, false, false)}
+                    {this.renderSwitch(17, 329, 198.38484, 86, switchHeight, false, true)}
+                    {this.renderSwitch(18, 329, 235.89022, 86, switchHeight, false, false)}
+                    {this.renderSwitch(19, 329, 251.9803, 86, switchHeight, false, true)}
+                    {this.renderSwitch(20, 329, 308.37741, 86, switchHeight, false, false)}
+                    {this.renderSwitch(21, 329, 325.18706, 221, switchHeight, false, true)}
+                    {this.renderSwitch(22, 329, 341.07932, 221, switchHeight, false, false)}
+                    {this.renderSwitch(23, 329, 356.28923, 221, switchHeight, false, true)}
+                    {this.renderSwitch(24, 329, 372.50665, 221, switchHeight, false, false)}
+                    {this.renderSwitch(25, 329, 389.77177, 221, switchHeight, false, true)}
+                    {this.renderSwitch(26, 329, 408.12294, 221, switchHeight, false, false)}
+                    {this.renderSwitch(27, 329, 464.4019, 86, switchHeight, false, true)}
+                    {this.renderSwitch(28, 329, 482.35716, 86, switchHeight, false, false)}
+                    {this.renderSwitch(29, 329, 523.64638, 86, switchHeight, false, true)}
 
                 </svg>               
             </Segment>
         )
     }
     colorForValue(value) {
+        const orange = '#efaa4f';
+        const green = '#8ee290';
+        const red = '#e55454';
         if (value > this.props.redZone) {
-            return "red"
+            return Color(red).hex();
         } else if (value > this.props.orangeZone) {
-            return "orange"
+            return Color(orange).mix(Color(red), (value - this.props.orangeZone) / (this.props.redZone - this.props.orangeZone)).hex();
+        } else if (value > this.props.greenZone) {
+            return Color(green).mix(Color(orange), (value - this.props.greenZone) / (this.props.orangeZone - this.props.greenZone)).hex();
         } else { 
-            return "green"
+            return Color(green).hex();
         }
     }
     renderChannel(channel, x,y,width, height, fill) {
@@ -1813,17 +1821,19 @@ class M3TriacTempsInSeries extends React.Component<{
             <svg>
                 <rect x={x} y={y} width={width} height={height} fill={fill} />
                 <text transform={`translate(${x} ${y})`} fill="white">
-                <tspan fontFamily="Courier" fontSize="5" style={{fontVariant: "italic", fontWeight:"bold", textAlign:"center"}} fill="white" x={width/2} y={height/2} textAnchor="middle" alignmentBaseline="central">{channel}</tspan>
+                <tspan fontFamily="Courier" fontSize="5" style={{fontVariant: "italic", fontWeight:"bold", textAlign:"center"}} fill="black" x={width/2} y={height/2} textAnchor="middle" alignmentBaseline="central">{channel}</tspan>
                 </text>
             </svg>
         )
     }
     renderSwitch(number, x, y,width, height, state, polarity) {
         var text = `Q${number}: ${valueWithUnit(this.props.values[`q${number}`], getUnitForTopic(this.props.channels[`q${number}`]))}`;
+        var temperatureColor = this.colorForValue(this.props.values[`q${number}`]);
         return (
             <svg>
-                <rect x={x} y={y} width={width} height={height} fill={this.colorForValue(this.props.values[`q${number}`])} strokeWidth={2} stroke={polarity ? "#bee2ba" : "#e2baba"} />
+                <rect x={x} y={y} width={width} height={height} fill={temperatureColor} strokeWidth={1.0} stroke={temperatureColor} />
                 <text transform={`translate(${x} ${y})`} fill="white">
+                <tspan fontFamily="Courier" fontSize="9" style={{fontVariant: "italic", fontWeight:"bold", textAlign:"center"}} fill="black" x={5} y={5} textAnchor="left" alignmentBaseline="central">{polarity ? '+' : '-'}</tspan>
                 <tspan fontFamily="Courier" fontSize="9" style={{fontVariant: "italic", fontWeight:"bold", textAlign:"center"}} fill="black" x={width/2} y={5} textAnchor="middle" alignmentBaseline="central">{text}</tspan>
                 </text>
             </svg>
