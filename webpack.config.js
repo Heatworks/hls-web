1314baaaf0fa725a8dc6a09b9d562cce3681bc6d
Weston Catron <weston@heatworks.tech>
2019-05-06 13:51:38 -0400

    Refactor, update weback.

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,93 @@
-'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var WebpackNotifierPlugin = require('webpack-notifier');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+
+var sourcePath = path.join(__dirname, './src');
+var outPath = path.join(__dirname, './build');
 
 module.exports = {
-    devtool: 'eval',
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        'react-hot-loader/patch',
-        'babel-polyfill',
-        path.join(__dirname, 'src/index.tsx')
-    ],
-    output: {
-        path: path.join(__dirname, '/build/'),
-        filename: '[name].js',
-        publicPath: '/'
+    context: sourcePath,
+    entry: {
+      index: path.join(__dirname, 'src', 'index.tsx'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: 'src/index.tpl.html',
-          inject: 'body',
-          filename: 'index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('development')
-        }),
-        new WebpackNotifierPlugin()
-    ],
     resolve: {
-      extensions: [ '.ts', '.tsx', '.js', '.jsx'],
-      modules: [path.resolve(__dirname, "src"), 'node_modules'],
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: outPath
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
     },
     module: {
         rules: [
-            { test: /\.tsx?$/, use: [
-                    {loader: 'babel-loader'},
-                    {loader: 'ts-loader'}
-                ] 
-            },
             {
-                test: /\.(jpe?g|png|gif|svg)$/i,
+                test: /\.(ts|tsx)?$/,
+                include: sourcePath,
                 use: [
-                    {
-                        loader: 'file-loader',
-                        options: {'hash':'sha512','digest':'hex','name':'[hash].[ext]'}
-                    },{
-                        loader: 'image-webpack-loader',
-                        options: {'bypassOnDebug': true}
-                    }
-                ]
-            }, { 
-                test: /\.css$/, 
-                use: [
-                     {
-                       loader: "style-loader"
-                     },
-                     {
-                       loader: "postcss-loader",
-                       options: {
-                         modules: true
-                       }
-                     }
+                    'ts-loader'
                 ]
             },
             {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader",
-                    publicPath: "/dist"
-                })
-            },
-            { 
-                test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, 
-                use: [{loader:'url-loader',
-                options: { limit: 10000, mimetype: 'application/font-woff' }}]
-            },
-            { 
-                test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, 
-                use: [{loader:'file-loader'}]
-            }
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+              'file-loader',
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  bypassOnDebug: true, // webpack@1.x
+                  disable: true, // webpack@2.x and newer
+                },
+              },
+            ],
+          },
+          // css
+          {
+            test: /\.css$/,
+            use: [
+              isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+              {
+                loader: 'css-loader',
+                query: {
+                  modules: true,
+                  sourceMap: !isProduction,
+                  importLoaders: 1,
+                  localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: [
+                    require('postcss-import')({ addDependencyTo: webpack }),
+                    require('postcss-url')(),
+                    require('postcss-preset-env')({
+                      /* use stage 2 features (defaults) */
+                      stage: 2,
+                    }),
+                    require('postcss-reporter')(),
+                    require('postcss-browser-reporter')({
+                      disabled: isProduction
+                    })
+                  ]
+                }
+              }
+            ]
+          },
+          // static assets
         ]
-    }
-};
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: './index.html'
+        })
+    ]
+}
